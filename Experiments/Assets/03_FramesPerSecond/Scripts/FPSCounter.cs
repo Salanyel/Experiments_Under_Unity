ce21d916bfb7a2146fc31b436915073b1dd17a4f
Xyunn <maxime.devecchi@gmail.com>
2017-04-03 14:25:48 +0200

    Adding a fps counter by creating protons and neutrons

---
+++ Experiments/Assets/03_FramesPerSecond/Scripts/FPSCounter.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+namespace _03_FramesPerSecond
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        public int averageFPS { get; private set; }
+        public int highestFPS { get; private set; }
+        public int lowestFPS { get; private set; }
+
+        public int frameRange = 60;
+
+        int[] _fpsBuffer;
+        int _fpsBufferIndex;
+
+        private void Awake()
+        {
+            InitializeBuffer();
+        }
+
+        private void Update()
+        {
+            if (frameRange <= 0)
+            {
+                InitializeBuffer();
+            }
+
+            UpdateBuffer();
+            CalculateFPS();
+            
+        }
+
+        /// <summary>
+        /// Initialize the FPS Buffer
+        /// </summary>
+        void InitializeBuffer()
+        {
+            if (frameRange <= 0)
+            {
+                frameRange = 1;
+            }
+
+            _fpsBuffer = new int[frameRange];
+            _fpsBufferIndex = 0;
+        }
+
+        /// <summary>
+        /// Update the FPS buffer
+        /// </summary>
+        void UpdateBuffer()
+        {
+            _fpsBuffer[_fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+
+            if (_fpsBufferIndex >= frameRange)
+            {
+                _fpsBufferIndex = 0;
+            }
+        }
+
+        /// <summary>
+        /// Calculate the current FPS by summing and dividing all values stored for a while
+        /// </summary>
+        void CalculateFPS()
+        {
+            int v_sum = 0;
+            int v_highest = 0;
+            int v_lowest = int.MaxValue;
+
+            for (int i = 0; i < frameRange; ++i)
+            {
+                int fps = _fpsBuffer[i];
+                v_sum += fps;
+
+                if (fps < v_lowest)
+                {
+                    v_lowest = fps;
+                }
+
+                if (fps > v_highest)
+                {
+                    v_highest = fps;
+                }
+
+            }
+            averageFPS = v_sum / frameRange;
+            highestFPS = v_highest;
+            lowestFPS = v_lowest;
+        }
+    }
+}
---
+++ Experiments/Assets/03_FramesPerSecond/Scripts/FPSDisplay.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _03_FramesPerSecond
+{
+    [RequireComponent(typeof(FPSCounter))]
+    public class FPSDisplay : MonoBehaviour
+    {
+
+        //Array from 0 to 99 to avoid any dynamic creation while setting a new arary (avoid to create the string "84" each frame for example)
+        static string[] stringsFromm00To99;
+
+        public Text fpsLabel, highestFpsLabel, lowestFpsLabel;
+
+        FPSCounter _fpsCounter;
+
+        [System.Serializable]
+        private struct FPSColor
+        {
+            public Color color;
+            public int minimumFPS;
+        }
+
+        [SerializeField]
+        private FPSColor[] _coloring;
+
+        private void Awake()
+        {
+            _fpsCounter = GetComponent<FPSCounter>();
+            stringsFromm00To99 = new string[100];
+
+            stringsFromm00To99[0] = "00";
+            stringsFromm00To99[1] = "01";
+            stringsFromm00To99[2] = "02";
+            stringsFromm00To99[3] = "03";
+            stringsFromm00To99[4] = "04";
+            stringsFromm00To99[5] = "05";
+            stringsFromm00To99[6] = "06";
+            stringsFromm00To99[7] = "07";
+            stringsFromm00To99[8] = "08";
+            stringsFromm00To99[9] = "09";
+
+            for (int i = 10; i < stringsFromm00To99.Length; ++i)
+            {
+                stringsFromm00To99[i] = i.ToString();
+            }
+        }
+
+        private void Update()
+        {
+            Display(fpsLabel, _fpsCounter.averageFPS);
+            Display(highestFpsLabel, _fpsCounter.highestFPS);
+            Display(lowestFpsLabel, _fpsCounter.lowestFPS);
+        }
+
+        /// <summary>
+        /// Color a UI Text depending on the FPS number displayed
+        /// </summary>
+        /// <param name="p_label"></param>
+        /// <param name="p_fps"></param>
+        private void Display(Text p_label, int p_fps)
+        {
+            p_label.text = stringsFromm00To99[Mathf.Clamp(p_fps, 0, 99)];
+            for (int i = 0; i < _coloring.Length; ++i)
+            {
+                if (p_fps >= _coloring[i].minimumFPS)
+                {
+                    p_label.color = _coloring[i].color;
+                    break;
+                }
+            }
+        }
+    }
+}
