8cf581f4260789ad88109d5a99448c817039f29d
spookyGitHub <contact@spooklight.ch>
2017-04-12 13:46:31 +0200

    Adding velocity & direction to the bezier curve

---
+++ Experiments/Assets/05_CurvesAndSplines/Scripts/Line.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _05_CurvesAndSplines
+{
+	public class Line : MonoBehaviour {
+
+		public Vector3 p0, p1;
+	}
+}
