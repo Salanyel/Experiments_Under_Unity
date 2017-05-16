509663b455d044898da227d7b7045a2923311ff2
spookyGitHub <contact@spooklight.ch>
2017-05-16 14:04:53 +0200

    Creating the scene for the 10_Rendering experiment

---
+++ Experiments/Assets/Global/NameFromButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NameFromButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text>().text = transform.parent.name;
+	}
+}
